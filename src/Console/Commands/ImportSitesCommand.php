bf45a4e0d9a12274b3bf8504a989e8859cf6b769
aude <aude.wiki@gmail.com>
2016-01-20 20:42:36 -0500

    Store sites config in a yaml file

--- src/Console/Commands/ImportSitesCommand.php
+++ src/Console/Commands/ImportSitesCommand.php
@@ -6,43 +6,51 @@ namespace MediaWiki\Sites\Console\Commands;
 use Filbertkm\Http\HttpClient;
 use Knp\Command\Command;
 use MediaWiki\Sites\Import\SiteMatrixSiteImporter;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Wikimedia\Assert\Assert;
 
 class ImportSitesCommand extends Command {
 
+	/**
+	 * @var HttpClient
+	 */
 	private $httpClient;
+
+	/**
+	 * @var string
+	 */
+	private $siteMatrix;
+
+	/**
+	 * @var string
+	 */
 	private $outputFile;
 
 	protected function configure() {
 		$this->setName( 'import-sites' )
-			->setDescription( 'Import sites' )
-			->addArgument(
-				'site-matrix',
-				InputArgument::REQUIRED,
-				'Site matrix api url'
-			);
+			->setDescription( 'Import sites' );
 	}
 
-	public function setServices( HttpClient $httpClient, $outputFile ) {
+	public function setServices( HttpClient $httpClient, $siteMatrix, $outputFile ) {
+		Assert::parameterType( 'string', $siteMatrix, '$siteMatrix' );
 		Assert::parameterType( 'string', $outputFile, '$outputFile' );
 
 		$this->httpClient = $httpClient;
+		$this->siteMatrix = $siteMatrix;
 		$this->outputFile = $outputFile;
 	}
 
 	protected function execute( InputInterface $input, OutputInterface $output ) {
-		$output->writeln( "Done" );
-
 		$importer = new SiteMatrixSiteImporter(
 			$this->httpClient,
-			$input->getArgument( 'site-matrix' ),
+			$this->siteMatrix,
 			$this->outputFile
 		);
 
 		$importer->import();
+
+		$output->writeln( "Done\n" );
 	}
 
 }
